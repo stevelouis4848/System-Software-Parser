68fbb9b6da4fe2d2323a719820c453bad73625c9
st105227 <st105227@eustis2>
2018-07-11 13:56:40 -0400

    work

--- hw3tc.c
+++ hw3tc.c
@@ -6,34 +6,71 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-FILE *ifp,ofp;
-char token[1024];
-
-ifp = fopen(".txt","r");
+int const MAX_NUM_TOKENS = 500;
+int const TOKEN_LENGTH = 11;
 	
+typedef enum{
+				nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
+				 multsym = 6,  slashsym = 7, oddsym = 8,  eqlsym = 9, neqsym = 10, 
+				 lessym = 11, leqsym = 12, 	gtrsym = 13, geqsym = 14, lparentsym = 15, 
+				 rparentsym = 16, commasym = 17, semicolonsym = 18, periodsym = 19, 
+				 becomessym = 20, beginsym = 21, endsym = 22, ifsym = 23, thensym = 24,
+				  whilesym = 25, dosym = 26, callsym = 27, constsym = 28, varsym = 29, 
+				  procsym = 30, writesym = 31, readsym = 32, elsesym = 33
+}tokenNames;
 
-int main(){
-	
+
+typedef struct token{
+
+				int type;
+				char value[MAX_NUM_TOKENS];
+}token;
+
+typedef struct enviroment{
+
+				token thisToken;
+				int currentIndex;
+				int arrayLength;
+}token;
+
+void readFile(){
+
 	int i;
-	FILE *ifp,ofp;
+	char buffer[MAX_NUM_TOKENS];
+	int buffer2;
+	enviroment *thisEnviroment;
+
+	FILE * ifp;
+ 	ifp = fopen("","r");
+
+	printf("fileName:%s\n",fileName);
+
+	enviroment = malloc(500 * sizeof(token));
+
+	thisEnviroment->arrayLength = MAX_NUM_TOKENS;
+	thisEnviroment->currentIndex = 0;
 
 
+	for(i = 0; fscanf(ifp, "%s", buffer) != EOF; i++){
+		
+		printf("buffer: %s\n", buffer);
 
+		buffer2 = atoi(buffer);
+		thisEnviroment[i]->thisToken.type = buffer;
 
-	ifp = fopen(".txt","r");
-	ofp = fopen(".txt","w");
+		if(buffer2 == 2 || buffer == 3){
 
+			fscanf(ifp, "%s", buffer);
+			strcopy(thisEnviroment[i]-> = thisToken.value, buffer);
+	}
 
-
-	
-	
+	block();		
 }
 
-char* getToken(){
+char* getToken(enviroment *thisEnviroment){
 
+	token = thisEnviroment->
 
-	fscanf(ifp, "%s", token)
-	
 	return Token;
 }
 
