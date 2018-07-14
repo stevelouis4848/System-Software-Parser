6818862ec9a0cf2f3e847bcb21bd293782a140e2
st105227 <st105227@eustis2>
2018-07-13 20:42:55 -0400

    work

--- compiler.c
+++ compiler.c
@@ -13,21 +13,25 @@ int main( int argc, char **argv){
 	int i;
 
 	for (i = 1;i < argc; i++){
-		/*
-		if(strcmp(argv[i], "-l")){
-			scannerVariable = 1;
+		
+		if(strcmp(argv[i], "-l") == 0){
+			printf("-l print lexical\n");
+			lexicalPrint++;
 		}
-		if(strcmp(argv[i], "-a")){
-			parserVariable = 0;
+		else if(strcmp(argv[i], "-a") == 0){
+			printf("-a print parser\n");
+			parserPrint++;
 		}
-		if(strcmp(argv[i], "-v")){
-			vmVariable = 0;
+		else if(strcmp(argv[i], "-v") == 0){
+			printf("-v print vms\n");
+			vmPrint++;
 		}
-		*/
+		else{
 			printf("running programs\n");
 			scanner(argv[i]);
 			program();
 			//vm();
+		}
 	}
 	printf("done\n");
 	return 0;
